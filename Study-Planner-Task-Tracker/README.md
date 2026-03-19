bf366bba883ad05f2ce8d7e897b9e903d4bdfb84
sherlyns138-crypto <sherly.n.s.138@kalvium.community>
2026-03-19 18:48:55 +0530

    Changed README.md file

---
+++ Study-Planner-Task-Tracker/README.md
@@ -0,0 +1,2 @@
+# Study-Planner-Task-Tracker 
+A web application designed to help students organize their academic tasks efficiently and boost their productivity.
--- Study-Planner-Task-Tracker/script.js
+++ Study-Planner-Task-Tracker/script.js
@@ -10,12 +10,12 @@ function addTask() {
 
   let li = document.createElement("li");
   li.innerHTML = `
-    ${task} (${subject}) - ${date}
-    <div>
-      <button onclick="markDone(this)">✔</button>
-      <button onclick="deleteTask(this)">❌</button>
-    </div>
-  `;
+  <span>📌 ${task} (${subject}) - ${date}</span>
+  <div>
+    <button onclick="markDone(this)">✔</button>
+    <button onclick="deleteTask(this)">❌</button>
+  </div>
+`;
 
   document.getElementById("taskList").appendChild(li);
 
@@ -32,3 +32,54 @@ function markDone(button) {
 function deleteTask(button) {
   button.parentElement.parentElement.remove();
 }
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  let button = document.getElementById("themeToggle");
+
+  if (document.body.classList.contains("dark-mode")) {
+    button.innerText = "☀ Light Mode";
+  } else {
+    button.innerText = "🌙 Dark Mode";
+  }
+}
+
+// Save theme
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  let isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+
+  let button = document.getElementById("themeToggle");
+  button.innerText = isDark ? "☀️" : "🌙";
+}
+
+// Load theme on page load
+window.onload = function () {
+  let savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    document.getElementById("themeToggle").innerText = "☀️";
+  }
+};
+
+function updateTaskCount() {
+  let total = document.querySelectorAll("#taskList li").length;
+  document.getElementById("taskCount").innerText = total + " tasks";
+}
+// After adding task
+updateTaskCount();
+
+// After deleting task
+updateTaskCount();
+
+function checkEmpty() {
+  let list = document.getElementById("taskList");
+  let message = document.getElementById("emptyMessage");
+
+  message.style.display = list.children.length === 0 ? "block" : "none";
+}
+checkEmpty();
